e85a3dac929997462468aaed5f9c72435c82bd60
AbaJabin <43990837+AbaJabin@users.noreply.github.com>
2020-05-29 15:48:41 +0530

    Added question

--- reverse_string.py
+++ reverse_string.py
@@ -1,3 +1,7 @@
+#2. Given a string of length N. Reverse the whole string without reversing the individual words in it.
+#Words are separated by dots using any library method.
+#Eg: String : Hello.world Output = world.Hello
+
 list = (input().split('.'))             #inputting he string and splitting it into words after each dot
 list = list[::-1]                       #order of the created list is reversed
 for words in list[:(len(list)-1)]:      #iterate through list to the secondlast word
